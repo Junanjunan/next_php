c6f693c6670d18bcb1f3b9ed9cee86433a741a50
Junanjunan <taltalrealty@gmail.com>
2024-07-05 15:00:21 +0900

    [fix] 로그인 방식 수정 - 하드코딩 > input 입력

--- src/app/components/LoginForm.js
+++ src/app/components/LoginForm.js
@@ -6,12 +6,14 @@ import axios from 'axios';
 const LoginForm = () => {
     const [response, setResponse] = useState(null);
     const [error, setError] = useState(null);
+    const [mbId, setMbId] = useState('');
+    const [password, setPassword] = useState('');
 
     const handlePostRequest = async () => {
         try {
             const result = await axios.post('http://localhost/login_next.php', {
-                mb_id: 'test',
-                password: '123'
+                mb_id: mbId,
+                password: password
             }, { withCredentials: true });
 
             setResponse(result.data);
@@ -23,6 +25,18 @@ const LoginForm = () => {
     return (
         <div>
             <h1>LOGIN - Request Example</h1>
+            <form>
+                <div>
+                    <label>
+                        Username: <input type="text" name="mb_id" value={mbId} onChange={(e) => setMbId(e.target.value)} />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Password: <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </label>
+                </div>
+            </form>
             <button onClick={handlePostRequest}>Send POST Request</button>
             {error && <div>Error: {error.message}</div>}
             {response && (
